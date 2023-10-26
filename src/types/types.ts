c0bcfd118f053fe3cd48a5413a41a5078d21bcaf
Erick Sposito <ericksposito@gmail.com>
2023-10-26 10:26:57 -0300

    consertando classe e adicionando constructor

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,12 +3,20 @@ aqui pretendo criar algumas definições de classes
 por hora o objetivo é basico e a idéia é ter uma página que demonstra os principais atributos de cada personagem
 aos poucos vou aprimorando para a adição de atributos por personagem de acordo com as builds de habilidades e futuramente os sets de armas e armaduas
 */
+class Classe{
+    nome: string;
+    forca: number;
+    inteligencia: number;
+    destreza: number;
+    determinacao: number;
 
-class Classe: object {
-    nome: string
-    forca: string
-    inteligencia: string
-    destreza: string
-    determinacao: string
+    constructor(nome: string, forca: number, inteligencia: number, destreza: number, determinacao: number){
+        this.nome = nome;
+        this.forca = forca;
+        this.inteligencia = inteligencia;
+        this.destreza = destreza;
+        this.determinacao = determinacao;
     }
+
+}
 
