64cdf7e5f6c03a466703b1b2d00337cd3d9ba87f
Erick Sposito <ericksposito@gmail.com>
2023-10-26 09:48:46 -0300

    inicializando projeto em typescript e rascunho de classe

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+/* 
+aqui pretendo criar algumas definições de classes
+por hora o objetivo é basico e a idéia é ter uma página que demonstra os principais atributos de cada personagem
+aos poucos vou aprimorando para a adição de atributos por personagem de acordo com as builds de habilidades e futuramente os sets de armas e armaduas
+*/
+
+class Classe: object {
+    nome: string
+    forca: string
+    inteligencia: string
+    destreza: string
+    determinacao: string
+    }
+
